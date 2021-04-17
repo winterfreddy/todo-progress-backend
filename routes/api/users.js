b5514ec88e1a07b6400f0389a44a0c31d6848580
Winfred Huang <huang.winfred21@gmail.com>
2021-04-16 22:26:34 -0700

    Add user token authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 // router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -23,11 +25,48 @@ router.post('/register', (req, res) => {
                         if (err) throw err;
                         newUser.password = hash;
                         newUser.save()
-                            .then(user => res.json(user))
+                            .then(user => {
+                                const payload = { id: user.id, username: user.username };
+
+                                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                                    res.json({
+                                        success: true,
+                                        token: "Bearer " + token
+                                    });
+                                });
+                            })
                             .catch(err => console.log(err));
                     })
                 })
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({email: 'This user does not exist'});
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = {id: user.id, username: user.username};
+
+                        jwt.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token
+                            });
+                        });
+                    } else {
+                        return res.status(400).json({password: 'Incorrect password'});
+                    }
+                })
         })
 })
 
